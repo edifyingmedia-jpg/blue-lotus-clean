e84b9e93f939b9693f57f194e3c2586b0baf0cd0
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-25 15:08:50 -0400

    Update Builder.jsx

--- frontend/src/builder/Builder.jsx
+++ frontend/src/builder/Builder.jsx
@@ -1,21 +1,11 @@
-import React, { useState } from "react";
-import Twin from "../twin/Twin";
+import React from "react";
+import { BuilderApp } from "./index.js";
+import { ProjectProvider } from "../state";
 
 export default function Builder() {
-  const [project, setProject] = useState({
-    name: "Untitled App",
-    pages: [],
-    components: []
-  });
-
-  function updateProject(next) {
-    setProject(prev => ({ ...prev, ...next }));
-  }
-
   return (
-    <div>
-      <h2>App Builder</h2>
-      <Twin project={project} updateProject={updateProject} />
-    </div>
+    <ProjectProvider>
+      <BuilderApp />
+    </ProjectProvider>
   );
 }
