2e48446a622185aec7bab86ca97ac3711c3dde1f
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-25 17:51:56 -0400

    Update engine.js

--- backend/twin/engine.js
+++ backend/twin/engine.js
@@ -10,6 +10,7 @@ import { getProject } from "./actions/getProject.js";
 import { createProject } from "./actions/createProject.js";
 import { updateProject } from "./actions/updateProject.js";
 import { deleteProject } from "./actions/deleteProject.js";
+import { listProjects } from "./actions/listProjects.js";
 
 export async function runTWIN(action, payload = {}) {
   try {
@@ -28,6 +29,9 @@ export async function runTWIN(action, payload = {}) {
 
       case "deleteProject":
         return await deleteProject(payload);
+
+      case "listProjects":
+        return await listProjects(payload);
 
       default:
         throw new Error(`Unknown TWIN action: ${action}`);
