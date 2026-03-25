14118acd6246519f5bf7ac7317f043301ec94d27
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-25 15:25:21 -0400

    Update index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,2 @@
-export { default as ComponentPanel } from "./ComponentPanel.jsx";
-export { default as ComponentRenderer } from "./ComponentRenderer.jsx";
+export * from "./ComponentRenderer.jsx";
 export * from "./ComponentRegistry.js";
