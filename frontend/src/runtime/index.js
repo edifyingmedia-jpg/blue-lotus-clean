eaf09622ed2020c9aec7ca34668ff42fd20efbc2
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-26 08:35:14 -0400

    Update index.js

--- frontend/src/runtime/index.js
+++ frontend/src/runtime/index.js
@@ -1,2 +1,18 @@
+// frontend/src/runtime/index.js
+
+/**
+ * Runtime Entry Point
+ * --------------------------------------------------
+ * Exports the full Blue Lotus runtime API.
+ * This allows the builder, preview engine, and
+ * external tools to import everything cleanly.
+ */
+
+export { default as LivePreview } from "./LivePreview";
 export { default as AppRenderer } from "./AppRenderer.jsx";
-export * from "./AppDefinitionValidator.js";
+export { default as PageRenderer } from "./PageRenderer.jsx";
+export { default as ComponentRenderer } from "./ComponentRenderer.jsx";
+
+export { RuntimeProvider, useRuntime } from "./RuntimeContext";
+
+export { getComponent } from "./ComponentRegistry";
