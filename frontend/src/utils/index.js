7dda0173d8ba622ea7137ce5aafd1359f92753c8
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-27 12:31:17 -0400

    Update index.js

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,2 +1,15 @@
-export * from "./id.js";
-export * from "./format.js";
+/**
+ * utils/index.js
+ * ----------------------------------------------------
+ * Central export hub for all utility functions used
+ * across the Blue Lotus builder + runtime environment.
+ *
+ * This keeps imports clean and ensures all utilities
+ * are discoverable from a single entrypoint.
+ */
+
+export { default as deepClone } from "./deepClone";
+export { default as eventBus } from "./eventBus";
+export { default as generateId } from "./generateId";
+export { default as safeGet } from "./safeGet";
+export { default as safeSet } from "./safeSet";
