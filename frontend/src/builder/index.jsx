3f7e2d8e6ebb3a6095bcfe4ae08e6505974dc271
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-28 16:36:19 -0400

    Update index.jsx

--- frontend/src/builder/index.jsx
+++ frontend/src/builder/index.jsx
@@ -1,22 +1,2 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import BuilderApp from "./BuilderApp";
-
-/**
- * Entry point for the Blue Lotus Builder.
- * This mounts the builder UI into the DOM.
- */
-
-const container = document.getElementById("root");
-
-if (!container) {
-  throw new Error("Missing #root element for Builder");
-}
-
-const root = createRoot(container);
-
-root.render(
-  <React.StrictMode>
-    <BuilderApp />
-  </React.StrictMode>
-);
+export { default as BuilderApp } from "./BuilderApp";
+export { default as ComponentPanel } from "./ComponentPanel";
