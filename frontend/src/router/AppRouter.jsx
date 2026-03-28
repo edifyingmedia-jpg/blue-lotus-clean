93a5d2e6e047cf0b1504cdcf37db1a800cd64adb
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-28 17:25:35 -0400

    Update AppRouter.jsx

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,34 +1,18 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import AppLayout from "../layout/AppLayout";
-import { ComponentPanel } from "../builder";
-import LivePreview from "../components/LivePreview";
-import Inspector from "../components/Inspector";
-import { useProject } from "../state";
+import BuilderApp from "../builder/BuilderApp";
 
 export default function AppRouter() {
-  const { project } = useProject();
-
   return (
     <BrowserRouter>
       <Routes>
         <Route
           path="/"
           element={
-            <AppLayout
-              left={<ComponentPanel addComponent={project.addComponent} />}
-              right={
-                <div style={{ display: "flex", height: "100%" }}>
-                  <div style={{ flex: 2, borderRight: "1px solid #ddd" }}>
-                    <LivePreview project={project} />
-                  </div>
-                  <div style={{ flex: 1 }}>
-                    <Inspector />
-                  </div>
-                </div>
-              }
-            />
+            <AppLayout>
+              <BuilderApp />
+            </AppLayout>
           }
         />
       </Routes>
