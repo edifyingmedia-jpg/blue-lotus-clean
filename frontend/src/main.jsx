9fcce80dff4715a9755e6e66420dbc346bf1a56f
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-29 10:45:05 -0400

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AppRouter from "./router/AppRouter";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AppRouter />
+    <App />
   </React.StrictMode>
 );
