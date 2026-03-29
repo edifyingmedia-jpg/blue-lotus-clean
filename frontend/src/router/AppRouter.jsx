61def89f4c41c2d023360662f915a564e40f6966
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-29 10:49:12 -0400

    Update AppRouter.jsx

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,28 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AppLayout from "../layout/AppLayout";
 import BuilderApp from "../builder/BuilderApp";
-import { ComponentPanel } from "../builder/ComponentPanel";
 
 export default function AppRouter() {
-  const [components, setComponents] = useState([]);
-
-  function handleAddComponent(component) {
-    setComponents((prev) => [...prev, component]);
-  }
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={
-            <AppLayout
-              left={<ComponentPanel onAddComponent={handleAddComponent} />}
-              right={<BuilderApp components={components} />}
-            />
-          }
-        />
+        <Route path="/*" element={<BuilderApp />} />
       </Routes>
     </BrowserRouter>
   );
