7e9d1608ba8ab9b0fb4c2ac756508cc63093d7bf
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-29 10:16:08 -0400

    Create AppRouter.jsx

---
+++ frontend/src/AppRouter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import BuilderApp from "./builder/BuilderApp";
+
+export default function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/*" element={<BuilderApp />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
