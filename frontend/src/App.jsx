fd8afe26ebc20231d167fae8cdbd0ad213cf2167
edifyingmedia-jpg <edifying.media@gmail.com>
2026-03-31 14:56:19 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,5 @@
 import SignInGate from "./auth/SignInGate";
-import TwinPanel from "./twin/TwinPanel";
 
 export default function App() {
-  return (
-    <SignInGate>
-      {(auth) => (
-        <TwinPanel
-          authority={{
-            isOwner: !!auth?.isOwner,
-            actorId: auth?.actorId ?? null,
-            ownerId: auth?.ownerId ?? null,
-            scope: auth?.isOwner ? "owner" : "user"
-          }}
-        />
-      )}
-    </SignInGate>
-  );
+  return <SignInGate />;
 }
