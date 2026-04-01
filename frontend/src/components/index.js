45379baa893407b4691f001b40a871fc317ce2fd
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-01 16:48:39 -0400

    Update index.js

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,3 +1,8 @@
 // frontend/src/components/index.js
 export { default as ComponentRenderer } from "./ComponentRenderer.jsx";
 export { componentRegistry } from "./ComponentRegistry.js";
+
+export default {
+  ComponentRenderer: require("./ComponentRenderer.jsx").default,
+  componentRegistry: require("./ComponentRegistry.js").componentRegistry,
+};
