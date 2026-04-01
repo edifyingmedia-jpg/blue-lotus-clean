8bf22b9928a896b30b8ab66c3a5bfd20cadf6559
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-01 17:14:24 -0400

    Update index.jsx

--- frontend/src/builder/index.jsx
+++ frontend/src/builder/index.jsx
@@ -1,1 +1,4 @@
+// frontend/src/builder/index.js
 export { generateBuilder } from "./generate";
+export { loadManifest } from "./registry";
+export default { generateBuilder: require("./generate").generateBuilder };
