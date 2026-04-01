6d7665cfc6b6f1be5e75bf3b0220b340522e4af1
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-01 17:02:04 -0400

    Update ComponentRegistry.js

--- frontend/src/components/ComponentRegistry.js
+++ frontend/src/components/ComponentRegistry.js
@@ -2,6 +2,10 @@
 import Button from "./Button";
 import Card from "./Card";
 
+/**
+ * Minimal registry mapping component names to React components.
+ * Add more components here as your project grows.
+ */
 export const componentRegistry = {
   Button,
   Card,
