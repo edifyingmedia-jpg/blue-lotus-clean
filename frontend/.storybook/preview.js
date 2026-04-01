d9a4f19e895185b83a4c438f68e71bc371aecb4d
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-01 18:48:58 -0400

    Create preview.js

---
+++ frontend/.storybook/preview.js
@@ -0,0 +1,18 @@
+// frontend/.storybook/preview.js
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/
+    }
+  },
+  backgrounds: {
+    default: "dark",
+    values: [
+      { name: "dark", value: "#0b1220" },
+      { name: "light", value: "#ffffff" }
+    ]
+  }
+};
