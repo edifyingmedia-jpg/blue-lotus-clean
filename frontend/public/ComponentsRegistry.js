2c5aa47e224fd05e058084b2fc054206f0b15acd
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-01 15:46:21 -0400

    Create ComponentsRegistry.js

---
+++ frontend/public/ComponentsRegistry.js
@@ -0,0 +1,10 @@
+// frontend/src/components/ComponentRegistry.js
+import Button from "./Button";
+import Card from "./Card";
+
+export const componentRegistry = {
+  Button,
+  Card,
+};
+
+export default componentRegistry;
