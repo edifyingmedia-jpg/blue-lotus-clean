4da28dd0ad7025f38433e693ee39771b5b64645f
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-01 12:34:05 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,25 +3,30 @@ import Workspace from "./builder/Workspace";
 
 export default function App() {
   return (
-    <div style={{ display: "flex", height: "100vh", width: "100vw" }}>
-      {/* TWIN PANEL CONTAINER */}
+    <div
+      style={{
+        display: "flex",
+        height: "100vh",
+        width: "100vw",
+        overflow: "hidden",
+      }}
+    >
+      {/* TWIN PANEL (AUTH + CONTROL) */}
       <div
         style={{
           width: "420px",
           display: "flex",
           flexDirection: "column",
           height: "100%",
-          borderRight: "1px solid #ddd",
-          background: "#000"
+          borderRight: "1px solid #222",
+          backgroundColor: "#000",
         }}
       >
         <SignInGate />
       </div>
 
-      {/* WORKSPACE */}
-      <div style={{ flex: 1, height: "100%" }}>
-        <Workspace />
-      </div>
+      {/* WORKSPACE (APP SURFACE) */}
+      <Workspace />
     </div>
   );
 }
