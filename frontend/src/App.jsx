1956e64e7f8ad9bf6a00edad5d736d321da3d3a8
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-02 16:32:29 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,5 @@
-import React from "react";
 import SignInGate from "./auth/SignInGate";
-import TwinPanel from "./components/TwinPanel";
 
 export default function App() {
-  return (
-    <div style={{ height: "100vh", width: "100vw", overflow: "hidden" }}>
-      <SignInGate>
-        <TwinPanel />
-      </SignInGate>
-    </div>
-  );
+  return <SignInGate />;
 }
