95a9e82a5a97873ab6d27c0e4af9fa345df61d02
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-02 14:17:39 -0400

    Update ActionTypes.js

--- frontend/src/runtime/actions/ActionTypes.js
+++ frontend/src/runtime/actions/ActionTypes.js
@@ -1,3 +1,14 @@
-export const ActionTypes = Object.freeze({
-  LOG_MESSAGE: "LOG_MESSAGE",
-});
+// ActionTypes.js
+// Defines the supported action types for the runtime ActionEngine.
+
+export const ACTION_SET_STATE = "setState";
+export const ACTION_NAVIGATE = "navigate";
+export const ACTION_CONDITIONAL = "conditional";
+export const ACTION_LOG = "log";
+
+export default {
+  ACTION_SET_STATE,
+  ACTION_NAVIGATE,
+  ACTION_CONDITIONAL,
+  ACTION_LOG,
+};
