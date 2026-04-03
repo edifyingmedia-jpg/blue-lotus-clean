e0fd6ad2a42a2e2eb4d8e064be0aa0e59f912102
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-03 14:59:07 -0400

    Update ComponentRegistry.js

--- frontend/src/runtime/ComponentRegistry.js
+++ frontend/src/runtime/ComponentRegistry.js
@@ -1,13 +1,39 @@
-import Text from "../components/primitives/Text";
-import Button from "../components/primitives/Button";
-import Input from "../components/primitives/Input";
-import Container from "../components/primitives/Container";
+/**
+ * ComponentRegistry.js
+ * ----------------------------------------------------
+ * Central registry mapping component type strings
+ * to the actual primitive React components.
+ *
+ * This is the modern, stable version aligned with:
+ * - rxgui/primitives/*
+ * - ComponentRenderer.jsx
+ * - ScreenRenderer.component.jsx
+ */
+
+import ActionText from "../rxgui/primitives/ActionText.component";
+import ActionButton from "../rxgui/primitives/ActionButton.component";
+import ActionInput from "../rxgui/primitives/ActionInput.component";
+import ActionImage from "../rxgui/primitives/ActionImage.component";
+import ActionContainer from "../rxgui/primitives/ActionContainer.component";
+import ActionList from "../rxgui/primitives/ActionList.component";
+import ActionSpacer from "../rxgui/primitives/ActionSpacer.component";
+import ActionDivider from "../rxgui/primitives/ActionDivider.component";
+import ActionGroup from "../rxgui/primitives/ActionGroup.component";
+import ActionCard from "../rxgui/primitives/ActionCard.component";
+import ActionToggle from "../rxgui/primitives/ActionToggle.component";
 
 const registry = {
-  Text,
-  Button,
-  Input,
-  Container,
+  text: ActionText,
+  button: ActionButton,
+  input: ActionInput,
+  image: ActionImage,
+  container: ActionContainer,
+  list: ActionList,
+  spacer: ActionSpacer,
+  divider: ActionDivider,
+  group: ActionGroup,
+  card: ActionCard,
+  toggle: ActionToggle,
 };
 
 export function getComponent(type) {
