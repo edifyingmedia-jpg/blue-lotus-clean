536fdb047de1fcac0ac597a19bf63d097dbdb5eb
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-02 20:31:44 -0400

    Create index.js

---
+++ frontend/src/runtime/utils/index.js
@@ -0,0 +1,9 @@
+// frontend/src/runtime/utils/index.js
+
+export { default as deepClone } from "./deepClone";
+export { default as eventBus } from "./eventBus";
+export { default as generateId } from "./generateId";
+export { default as logger } from "./logger";
+export { default as safeGet } from "./safeGet";
+export { default as safeSet } from "./safeSet";
+export { default as validator } from "./validator";
