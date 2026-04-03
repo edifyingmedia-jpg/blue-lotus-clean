3a843741d27e5214a78ed155299967d396c205ea
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-03 14:12:24 -0400

    Create ActionImage.component.jsx

---
+++ frontend/src/rxgui/primitives/ActionImage.component.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { ActionEngine } from "../runtime/ActionEngine";
+
+export default function ActionImage({
+  src = "",
+  alt = "",
+  action = null,
+  params = {},
+  width = "100%",
+  height = "auto",
+  margin = "0 0 12px 0",
+  borderRadius = 6,
+  objectFit = "cover",
+}) {
+  const handleClick = () => {
+    if (!action) return;
+
+    try {
+      const engine = new ActionEngine();
+      engine.run(action, params);
+    } catch (err) {
+      console.error("ActionImage error:", err);
+    }
+  };
+
+  return (
+    <img
+      src={src}
+      alt={alt}
+      onClick={handleClick}
+      style={{
+        width,
+        height,
+        margin,
+        borderRadius,
+        objectFit,
+        cursor: action ? "pointer" : "default",
+      }}
+    />
+  );
+}
