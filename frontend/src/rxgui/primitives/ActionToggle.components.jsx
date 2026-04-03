5a9c4e39c08564c7f3f53d9a995e0a9a2bdaf966
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-03 16:30:03 -0400

    Update ActionToggle.components.jsx

--- frontend/src/rxgui/primitives/ActionToggle.components.jsx
+++ frontend/src/rxgui/primitives/ActionToggle.components.jsx
@@ -1,36 +1,15 @@
 import React from "react";
-import { ActionEngine } from "../runtime/ActionEngine";
 
-export default function ActionToggle({
-  value = false,
-  action = null,
-  params = {},
-  label = "",
-  margin = "0 0 12px 0",
-  fontSize = 16,
-}) {
-  const handleChange = () => {
-    try {
-      const engine = new ActionEngine();
-      engine.run(action, { ...params, value: !value });
-    } catch (err) {
-      console.error("ActionToggle error:", err);
-    }
-  };
-
+export default function ActionToggle({ checked = false, onChange, className, ...rest }) {
   return (
-    <label
-      style={{
-        display: "flex",
-        alignItems: "center",
-        gap: "8px",
-        margin,
-        fontSize,
-        cursor: "pointer",
-      }}
+    <button
+      type="button"
+      className={className}
+      aria-pressed={!!checked}
+      onClick={() => onChange && onChange(!checked)}
+      {...rest}
     >
-      <input type="checkbox" checked={value} onChange={handleChange} />
-      {label}
-    </label>
+      {checked ? "On" : "Off"}
+    </button>
   );
 }
