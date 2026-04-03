e4bea2ac62e8b8ec1234d034460dc658196f8a95
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-03 18:53:41 -0400

    Update index.js

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,2 +1,10 @@
 export * from "./format.js";
 export * from "./id.js";
+
+export { default as deepClone } from "./deepClone.js";
+export { default as eventBus } from "./eventBus.js";
+export { default as generateId } from "./generateId.js";
+export { default as logger } from "./logger.js";
+export { default as safeGet } from "./safeGet.js";
+export { default as safeSet } from "./safeSet.js";
+export { default as validator } from "./validator.js";
