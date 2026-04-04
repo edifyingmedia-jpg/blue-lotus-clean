7f2e420e411c5ffb7bc2921c1ae2eeff7b2a9101
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-04 10:50:32 -0400

    Update index.js

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,10 +1,12 @@
-export * from "./format.js";
-export * from "./id.js";
+export const safeSet = (obj, path, value) => {
+  const keys = Array.isArray(path) ? path : path.split('.');
+  const lastKey = keys.pop();
 
-export { default as deepClone } from "./deepClone.js";
-export { default as eventBus } from "./eventBus.js";
-export { default as generateId } from "./generateId.js";
-export { default as logger } from "./logger.js";
-export { default as safeGet } from "./safeGet.js";
-export { default as safeSet } from "./safeSet.js";
-export { default as validator } from "./validator.js";
+  const target = keys.reduce((acc, key) => {
+    if (!acc[key]) acc[key] = {};
+    return acc[key];
+  }, obj);
+
+  target[lastKey] = value;
+  return obj;
+};
