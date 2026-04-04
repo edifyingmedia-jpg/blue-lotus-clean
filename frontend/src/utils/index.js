af2f2ad2f1bf3c7507a2747de67efbfb134a31ac
edifyingmedia-jpg <edifying.media@gmail.com>
2026-04-04 11:04:35 -0400

    Update index.js

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,12 +1,4 @@
-export const safeSet = (obj, path, value) => {
-  const keys = Array.isArray(path) ? path : path.split('.');
-  const lastKey = keys.pop();
-
-  const target = keys.reduce((acc, key) => {
-    if (!acc[key]) acc[key] = {};
-    return acc[key];
-  }, obj);
-
-  target[lastKey] = value;
-  return obj;
-};
+export * from './findNodeById.js';
+export * from './format.js';
+export * from './id.js';
+export * from './safeSet.js';
