401d8406cf731afbdb79a8f514c288b6510f2c9b
Tiffany <edifying.media@gmail.com>
2026-04-16 10:10:55 -0400

    Update index.js

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,1 +1,20 @@
+// frontend/src/pages/index.js
+
+/** * BLUE LOTUS PAGE REGISTRY 
+ * -------------------------
+ * Centrally managing all public and authenticated actuation nodes.
+ */
+
+// Core Rendering Engine
 export { default as PageRenderer } from "./PageRenderer.jsx";
+
+// Future Empire Modules (Ready for Actuation)
+// export { default as Storefront } from "./Storefront.jsx";
+// export { default as MemberProfile } from "./MemberProfile.jsx";
+// export { default as RevenueDashboard } from "./RevenueDashboard.jsx";
+
+/**
+ * ARCHITECT NOTE:
+ * All new public-facing nodes should be registered here to ensure
+ * they inherit the Ink & Cyan global shell and 10% Tax tracking.
+ */
