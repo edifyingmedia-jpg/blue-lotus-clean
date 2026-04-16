75b3d83738ed4ca8f465ecac180512891abd08fb
Tiffany <edifying.media@gmail.com>
2026-04-16 02:53:47 -0400

    Update main.js

--- frontend/.storybook/main.js
+++ frontend/.storybook/main.js
@@ -1,16 +1,21 @@
 // frontend/.storybook/main.js
 module.exports = {
-  stories: [
-    "../src/**/*.stories.@(js|jsx)"
-  ],
+  stories: ["../src/**/*.stories.@(js|jsx)"],
   addons: [
-    "@storybook/addon-essentials"
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions", // To test AI-driven clicks
+    {
+      name: "@storybook/addon-styling",
+      options: {
+        postCss: true, // This enables Tailwind support in Storybook
+      },
+    },
   ],
   framework: {
-    name: "@storybook/react",
-    options: {}
+    name: "@storybook/react-vite", // Vite is standard for your 2026 stack
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
