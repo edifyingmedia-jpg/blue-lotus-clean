fc339456ebeb621d8e556cb64fbb6930d8ba159e
Tiffany <edifying.media@gmail.com>
2026-04-16 10:07:52 -0400

    Update index.js

--- frontend/src/layout_TEMPs/index.js
+++ frontend/src/layout_TEMPs/index.js
@@ -1,1 +1,10 @@
+// frontend/src/layout_TEMPs/index.js
+
+/**
+ * DEPRECATED_NODE: Legacy Layout Registry
+ * --------------------------------------
+ * This directory is marked for sunset.
+ * Use /src/layout for the prime Empire shell.
+ */
+
 export { default as BuilderLayout } from "./BuilderLayout";
