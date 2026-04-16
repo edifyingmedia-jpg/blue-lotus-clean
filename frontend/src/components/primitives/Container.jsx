a8d97e22b6f22644aa60580adc1c088bb4594bdb
Tiffany <edifying.media@gmail.com>
2026-04-16 00:38:45 -0400

    Update Container.jsx

--- frontend/src/components/primitives/Container.jsx
+++ frontend/src/components/primitives/Container.jsx
@@ -1,21 +1,19 @@
+// frontend/src/components/primitives/Container.jsx
 import React from "react";
 
-export default function Container({
-  children,
-  padding = 0,
-  margin = 0,
-  maxWidth = "100%",
-  style = {}
+export default function Container({ 
+  children, 
+  className = "", 
+  style = {},
+  maxWidth = "none" 
 }) {
   return (
-    <div
-      style={{
-        padding,
-        margin,
-        maxWidth,
-        boxSizing: "border-box",
-        ...style
-      }}
+    <div 
+      className={`w-full mx-auto box-border ${className}`}
+      style={{ 
+        maxWidth: maxWidth !== "none" ? maxWidth : undefined,
+        ...style 
+      }} 
     >
       {children}
     </div>
