5b62dfb993117f63f38eadd876ea903a809525cd
Tiffany <edifying.media@gmail.com>
2026-04-16 10:21:26 -0400

    Update ActionSpacer.component.jsx

--- frontend/src/rxgui/primitives/ActionSpacer.component.jsx
+++ frontend/src/rxgui/primitives/ActionSpacer.component.jsx
@@ -1,15 +1,33 @@
+// frontend/src/rxgui/primitives/ActionSpacer.component.jsx
 import React from "react";
 
-export default function ActionSpacer({
-  height = 12,
-  width = "100%",
+/**
+ * ActionSpacer (Empire Edition)
+ * ----------------------------
+ * A structural void node for Blue Lotus modules.
+ * Enforces industrial spacing scales instead of arbitrary pixels.
+ */
+export default function ActionSpacer({ 
+  size = "md", // sm, md, lg, xl
+  axis = "vertical" 
 }) {
+  const scales = {
+    sm: "h-4 w-4",   // 16px
+    md: "h-8 w-8",   // 32px
+    lg: "h-16 w-16", // 64px
+    xl: "h-32 w-32", // 128px
+  };
+
+  const isVertical = axis === "vertical";
+
   return (
-    <div
-      style={{
-        height,
-        width,
-      }}
+    <div 
+      className={`
+        ${isVertical ? scales[size].split(' ')[0] : scales[size].split(' ')[1]}
+        ${isVertical ? "w-full" : "h-full"}
+        flex-shrink-0 transition-all duration-300
+      `} 
+      aria-hidden="true"
     />
   );
 }
