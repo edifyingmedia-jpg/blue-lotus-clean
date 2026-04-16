b817643a39135ea98076c57de5ab2d5ff3d19d84
Tiffany <edifying.media@gmail.com>
2026-04-16 01:32:52 -0400

    Update index.jsx

--- frontend/src/builder/index.jsx
+++ frontend/src/builder/index.jsx
@@ -1,4 +1,20 @@
 // frontend/src/builder/index.js
 
+// Core Logic & Registry
 export { generateBuilder } from "./generate";
 export { RegistryV2 } from "./components/registry.jsx";
+export { builderSpecSchema } from "./builderSpecSchema";
+
+// Main Layout Containers
+export { default as BuilderApp } from "./BuilderApp";
+export { default as Workspace } from "./Workspace";
+export { default as BuilderShell } from "./BuilderShell";
+
+// Rendering & Panels
+export { default as CanvasRenderer } from "./CanvasRenderer";
+export { default as ComponentRenderer } from "./ComponentRenderer";
+export { default as ComponentPanel } from "./ComponentPanel";
+export { default as AppStatusPanel } from "./AppStatusPanel";
+
+// Factories
+export { createBuilderSpec, instantiateBuilderFromSpec } from "./BuilderFactory";
